7eef55b78e0b7d42bcb75eabe387353b464aed10
white-sss <shoko.work.1029@gmail.com>
2025-03-03 18:08:55 +0900

    Docs

---
+++ docs/x-swap/index.md
@@ -0,0 +1,20 @@
+---
+sidebar_label: X-Swapについて
+sidebar_position: 1
+---
+
+# X-Swapとは？
+
+X-Swapは、異なるネットワーク上の **ステーブルコイン（USDTX、USDCX）をスワップ（交換）** できる機能です。
+
+これにより、ユーザーはネットワークをまたいで流動性を確保し、資産管理をより自由に行うことができます。
+
+### **対応しているネットワーク**
+
+X-Swapは、X-Gateと同様に **5つの主要なブロックチェーン** に対応しています。
+
+- Ethereum
+- Japan Open Chain
+- Arbitrum
+- Base
+- Avalanche
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,14 +86,14 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       footer: {
